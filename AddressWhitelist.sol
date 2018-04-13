75ab53b2fb75c2a04ec06af2aa0ae0898f3a87d4
EtherTote <36612007+ethertote@users.noreply.github.com>
2018-04-13 21:22:45 +0100

    Add files via upload

---
+++ AddressWhitelist.sol
@@ -0,0 +1,50 @@
+// Ethertote.com - optional customer whitelist contract - dev to discuss option
+
+// full credit for contract and functions goes to Bounty0x
+// github.com/bounty0x/Bounty0xCrowdsale/contracts/AddressWhitelist.sol
+
+
+pragma solidity ^0.4.21;
+
+import "./Ownable.sol';
+
+// A simple contract that stores a whitelist of addresses, which the owner may update
+contract AddressWhitelist is Ownable {
+    // the addresses that are included in the whitelist
+    mapping (address => bool) public whitelisted;
+
+    function AddressWhitelist() public {
+    }
+
+    function isWhitelisted(address addr) view public returns (bool) {
+        return whitelisted[addr];
+    }
+
+    event LogWhitelistAdd(address indexed addr);
+
+    // add these addresses to the whitelist
+    function addToWhitelist(address[] addresses) public onlyOwner returns (bool) {
+        for (uint i = 0; i < addresses.length; i++) {
+            if (!whitelisted[addresses[i]]) {
+                whitelisted[addresses[i]] = true;
+                LogWhitelistAdd(addresses[i]);
+            }
+        }
+
+        return true;
+    }
+
+    event LogWhitelistRemove(address indexed addr);
+
+    // remove these addresses from the whitelist
+    function removeFromWhitelist(address[] addresses) public onlyOwner returns (bool) {
+        for (uint i = 0; i < addresses.length; i++) {
+            if (whitelisted[addresses[i]]) {
+                whitelisted[addresses[i]] = false;
+                LogWhitelistRemove(addresses[i]);
+            }
+        }
+
+        return true;
+    }
+}
