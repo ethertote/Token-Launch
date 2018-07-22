c11ae2cb628fa5aa77ec6877b759de919c067648
EtherTote <36612007+ethertote@users.noreply.github.com>
2018-07-22 23:42:59 +0100

    Update TeamEth.sol

--- testing/TeamEth.sol
+++ testing/TeamEth.sol
@@ -1,54 +1,59 @@
 // Ethertote - Team Eth timelocked smart contract
+//
+// The following contract offers peace of mind to investors as the
+// Eth that will go to the members of the Ethertote team
+// will be time-locked over a 12-month period whereby
+// The withdraw functions can only be called when the current timestamp is 
+// greater than the time specified in each functions
+// ----------------------------------------------------------------------------
 
 pragma solidity 0.4.24;
 
 import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "github.com/ethertote/core/SafeMath.sol";
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract TeamEth {
     using SafeMath for uint256;
 
-    address public creator;
-    address public owner;
+    address public thisContractAddress;
+    address public admin;
     
-    // times/dates that Eth can be withdrawn 
+    // time expressed in seconds of 3 months (1 quarter of a year)
+    uint public oneQuarterInSeconds = 7890000;
+    
+    
+    // the first team withdrawal can be made after December 1st 2018
     // expressed as Unix epoch time 
     // https://www.epochconverter.com/
-    uint256 public unlockDate1;
-    uint256 public unlockDate2;
-    uint256 public unlockDate3;
-    uint256 public unlockDate4;
+    uint256 public unlockDate1 = 1543622400;
+    
+    uint256 public unlockDate2 = unlockDate1.add(oneQuarterInSeconds);
+    uint256 public unlockDate3 = unlockDate2.add(oneQuarterInSeconds);
+    uint256 public unlockDate4 = unlockDate3.add(oneQuarterInSeconds);
+    
     uint256 public createdAt;
     
     // percentage that the team can withdraw Eth
+    // it can naturally be inferred that quarter4 will be 25%
     uint public quarter1 = 25;
     uint public quarter2 = 25;
-    uint public quarter3 = 25; 
-    
+    uint public quarter3 = 25;
 
-    modifier onlyOwner {
-        require(msg.sender == owner);
+    event Received(address from, uint256 amount);
+    event Withdrew(address to, uint256 amount);
+    
+    modifier onlyAdmin {
+        require(msg.sender == admin);
         _;
     }
 
-    constructor (
-        address _creator,
-        address _owner,
-        uint256 _unlockDate1,
-        uint256 _unlockDate2,
-        uint256 _unlockDate3,
-        uint256 _unlockDate4
-    ) public {
-        creator = _creator;
-        owner = _owner;
-        unlockDate1 = _unlockDate1;
-        unlockDate2 = _unlockDate2;
-        unlockDate3 = _unlockDate3;
-        unlockDate4 = _unlockDate4;
+    constructor () public {
+        admin = msg.sender;
+        thisContractAddress = address(this);
         createdAt = now;
     }
 
-    // keep all the ether sent to this address
+    // fallback to store all the ether sent to this address
     function() payable public { 
         emit Received(msg.sender, msg.value);
     }
@@ -57,57 +62,54 @@ contract TeamEth {
         return address(this).balance;
     }
 
-    // callable by owner only, after specified time
-    function withdraw1() onlyOwner public {
+    // team withdrawal after specified time
+    function withdraw1() onlyAdmin public {
        require(now >= unlockDate1);
        // now allow a percentage of the balance
        msg.sender.transfer((address(this).balance).div(quarter1));
        emit Withdrew(msg.sender, (address(this).balance).div(quarter1));    // 25%
     }
     
-        // callable by owner only, after specified time
-    function withdraw2() onlyOwner public {
+    // team withdrawal after specified time
+    function withdraw2() onlyAdmin public {
        require(now >= unlockDate2);
        // now allow a percentage of the balance
        msg.sender.transfer((address(this).balance).div(quarter2));
        emit Withdrew(msg.sender, (address(this).balance).div(quarter2));    // 25%
     }
     
-        // callable by owner only, after specified time
-    function withdraw3() onlyOwner public {
+    // team withdrawal after specified time
+    function withdraw3() onlyAdmin public {
        require(now >= unlockDate3);
        // now allow a percentage of the balance
        msg.sender.transfer((address(this).balance).div(quarter3));
        emit Withdrew(msg.sender, (address(this).balance).div(quarter3));    // 25%
     }
     
-        // callable by owner only, after specified time
-    function withdraw4() onlyOwner public {
+    // team withdrawal after specified time
+    function withdraw4() onlyAdmin public {
        require(now >= unlockDate4);
        // now allow all remaining balance
        msg.sender.transfer(address(this).balance);
-       emit Withdrew(msg.sender, address(this).balance);    // remaining balance
+       emit Withdrew(msg.sender, address(this).balance);    // all remaining balance
     }
 
 
-    function infoWithdraw1() public view returns(address, address, uint256, uint256, uint256) {
-        return (creator, owner, unlockDate1, createdAt, address(this).balance);
+    function infoWithdraw1() public view returns(address, uint256, uint256, uint256) {
+        return (admin, unlockDate1, createdAt, address(this).balance);
     }
 
-    function infoWithdraw2() public view returns(address, address, uint256, uint256, uint256) {
-        return (creator, owner, unlockDate2, createdAt, address(this).balance);
-    }
-    
-    function infoWithdraw13() public view returns(address, address, uint256, uint256, uint256) {
-        return (creator, owner, unlockDate3, createdAt, address(this).balance);
+    function infoWithdraw2() public view returns(address, uint256, uint256, uint256) {
+        return (admin, unlockDate2, createdAt, address(this).balance);
     }
     
-    function infoWithdraw4() public view returns(address, address, uint256, uint256, uint256) {
-        return (creator, owner, unlockDate4, createdAt, address(this).balance);
+    function infoWithdraw13() public view returns(address, uint256, uint256, uint256) {
+        return (admin, unlockDate3, createdAt, address(this).balance);
+    }
+    
+    function infoWithdraw4() public view returns(address, uint256, uint256, uint256) {
+        return (admin, unlockDate4, createdAt, address(this).balance);
     }
 
 
-
-    event Received(address from, uint256 amount);
-    event Withdrew(address to, uint256 amount);
 }
