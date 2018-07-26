9d269c20725aba89985f825ec6cb85c0a860979f
EtherTote <36612007+ethertote@users.noreply.github.com>
2018-07-27 00:44:32 +0100

    Update TeamEth.sol

--- TeamEth.sol
+++ TeamEth.sol
@@ -9,7 +9,58 @@
 
 pragma solidity 0.4.24;
 
-import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+///////////////////////////////////////////////////////////////////////////////
+// SafeMath Library 
+///////////////////////////////////////////////////////////////////////////////
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Main contract
+//////////////////////////////////////////////////////////////////////////////
 
 contract TeamEth {
     using SafeMath for uint256;
@@ -70,7 +121,6 @@ contract TeamEth {
 
     // fallback to store all the ether sent to this address
     function() payable public { 
-        emit Received(msg.sender, msg.value);
     }
     
     function thisContractBalance() public view returns(uint) {
@@ -119,8 +169,7 @@ contract TeamEth {
        require(now >= unlockDate4);
        // now allow all remaining balance to be claimed
        msg.sender.transfer(address(this).balance);
-       emit Withdrew(msg.sender, address(this).balance);           // all remaining balance
+       emit Withdrew(msg.sender, address(this).balance);    // all remaining balance
     }
-
 
 }
